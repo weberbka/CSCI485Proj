d80d28b8a7da9a55b10f711d75e30ab1da9f7666
weberbka <56563711+weberbka@users.noreply.github.com>
2020-11-11 20:10:42 -0800

    added quest marker to NPC

---
+++ Assets/Scripts/isNPCInteractable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isNPCInteractable : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject quest;
+    // Start is called before the first frame update
+    void Start()
+    {
+		
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 position = this.transform.position;
+		position.y -= (float) (GetComponent<SpriteRenderer>().bounds.size.y*0.85);
+        if(Vector3.Distance(player.transform.position, position) < 2) quest.transform.gameObject.SetActive(true);
+		else quest.transform.gameObject.SetActive(false);
+    }
+}
