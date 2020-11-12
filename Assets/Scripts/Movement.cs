93dac458854a07b0532baeae81399eb3f30d077e
weberbka <56563711+weberbka@users.noreply.github.com>
2020-11-11 18:16:18 -0800

    Movement script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour
+{
+	public Animator anima;
+	const float SPEED = 0.01f;
+	bool moveDown = false;
+	bool moveLeft = false;
+	bool moveUp = false;
+	bool moveRight = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyUp(KeyCode.LeftArrow))
+		{
+			anima.SetBool("left", false);
+			moveLeft = false;
+		}
+		if (Input.GetKeyUp(KeyCode.RightArrow))
+		{
+			anima.SetBool("right", false);
+			moveRight = false;
+		}
+		if (Input.GetKeyUp(KeyCode.UpArrow))
+		{
+			anima.SetBool("up", false);
+			moveUp = false;
+		}
+		if (Input.GetKeyUp(KeyCode.DownArrow))
+		{
+			anima.SetBool("down", false);
+			moveDown = false;
+		}
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			anima.SetBool("left", true);
+			moveLeft = true;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			anima.SetBool("right", true);
+			moveRight = true;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			anima.SetBool("up", true);
+			moveUp = true;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			anima.SetBool("down", true);
+			moveDown = true;
+		}
+		
+		
+		if (moveLeft && moveUp)
+		{
+			Vector3 position = this.transform.position;
+			position.x -= (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			position.y += (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			this.transform.position = position;
+		}
+		else if (moveRight && moveDown)
+		{
+			Vector3 position = this.transform.position;
+			position.x += (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			position.y -= (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			this.transform.position = position;
+		}
+		else if (moveUp && moveRight)
+		{
+			Vector3 position = this.transform.position;
+			position.x += (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			position.y += (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			this.transform.position = position;
+		}
+		else if (moveDown && moveLeft)
+		{
+			Vector3 position = this.transform.position;
+			position.x -= (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			position.y -= (float) Math.Sqrt(Math.Pow(SPEED, 2)/2);
+			this.transform.position = position;
+		}
+        else if (moveLeft)
+		{
+			Vector3 position = this.transform.position;
+			position.x -= SPEED;
+			this.transform.position = position;
+		}
+		else if (moveRight)
+		{
+			Vector3 position = this.transform.position;
+			position.x += SPEED;
+			this.transform.position = position;
+		}
+		else if (moveUp)
+		{
+			Vector3 position = this.transform.position;
+			position.y += SPEED;
+			this.transform.position = position;
+		}
+		else if (moveDown)
+		{
+			Vector3 position = this.transform.position;
+			position.y -= SPEED;
+			this.transform.position = position;
+		}
+    }
+}
