3b28553e402d7cb6764299220e182536918586cf
weberbka <56563711+weberbka@users.noreply.github.com>
2020-12-04 23:36:39 -0800

    Fixed Interaction script + updated quest marker

--- Assets/Scripts/Interaction Scripts/NPCInteraction.cs
+++ Assets/Scripts/Interaction Scripts/NPCInteraction.cs
@@ -14,28 +14,34 @@ public class NPCInteraction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-		foreach (Transform child in this.transform) child.gameObject.SetActive(false);
-		//quest = this.gameObject.transform.GetChild(0).gameObject;
-		//quest.transform.gameObject.SetActive(false);
+		//foreach (Transform child in this.transform) child.gameObject.SetActive(false);
 		player = GameObject.Find("Player");
 		this.gameObject.tag = "Interactable";
+		//questScript.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+		// if(hasQuest){
+			// Vector3 position = this.transform.position;
+			// position.y -= (float) (GetComponent<SpriteRenderer>().bounds.size.y*0.85);
+			// int count = 0;
+			// active = false;
+			// foreach (Transform child in transform){
+				// child.gameObject.SetActive(false);
+				// if(count == dialoguePart && Vector3.Distance(player.transform.position, position) < 2){
+					// child.gameObject.SetActive(true);
+					// active = true;
+				// }
+				// count++;
+			// }
+		// }
 		if(hasQuest){
 			Vector3 position = this.transform.position;
 			position.y -= (float) (GetComponent<SpriteRenderer>().bounds.size.y*0.85);
-			int count = 0;
-			active = false;
-			foreach (Transform child in transform){
-				child.gameObject.SetActive(false);
-				if(count == dialoguePart && Vector3.Distance(player.transform.position, position) < 2){
-					child.gameObject.SetActive(true);
-					active = true;
-				}
-				count++;
+			if(Vector3.Distance(player.transform.position, position) < 2){
+				//questScript.enabled = true;
 			}
 		}
     }
---
+++ Assets/Scripts/Interaction Scripts/ResourceCollector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceCollector : MonoBehaviour
+{
+	public GameObject player;
+	
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.Find("Player"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = this.transform.position;
+		position.y -= (float) (GetComponent<SpriteRenderer>().bounds.size.y*0.85);
+		if(Vector3.Distance(player.transform.position, position) < 2){
+			this.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+		}else this.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
